8c69533eb556147b910b0869d7ce0eadeebb1b81
noval assalam <naufalassalam@gmail.com>
2021-12-09 10:12:22 +0700

    comvis

--- pw.sql
+++ pw.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: 24 Agu 2019 pada 04.40
--- Versi Server: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Host: localhost
+-- Waktu pembuatan: 09 Des 2021 pada 04.12
+-- Versi server: 10.4.18-MariaDB
+-- Versi PHP: 7.4.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,6 +20,27 @@ SET time_zone = "+00:00";
 --
 -- Database: `pw`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `comvis`
+--
+
+CREATE TABLE `comvis` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `akurasi` float NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `comvis`
+--
+
+INSERT INTO `comvis` (`id`, `nama`, `akurasi`, `time`) VALUES
+(62, 'kucing', 0.90779, '2021-12-09 02:26:02'),
+(63, 'kucing', 0.907297, '2021-12-09 03:06:11');
 
 -- --------------------------------------------------------
 
@@ -58,8 +80,11 @@ CREATE TABLE `kota` (
 --
 
 INSERT INTO `kota` (`id_kota`, `nama_kota`) VALUES
+(3, 'aceh'),
 (1, 'bandung'),
-(2, 'jakarta');
+(2, 'jakarta'),
+(6, 'jepang'),
+(5, 'tttegal');
 
 -- --------------------------------------------------------
 
@@ -112,85 +137,103 @@ CREATE TABLE `user` (
   `nama_user` varchar(40) NOT NULL,
   `password_user` varchar(70) NOT NULL,
   `id_kota` int(11) NOT NULL,
-  `email_user` varchar(50) NOT NULL
+  `email_user` varchar(50) NOT NULL,
+  `level` enum('1','2') NOT NULL COMMENT '1 super 2 admin'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user` (`id_user`, `nama_user`, `password_user`, `id_kota`, `email_user`) VALUES
-(1, 'noval', '21232f297a57a5a743894a0e4a801fc3', 1, 'admin@gmail.com'),
-(2, 'admin', '21232f297a57a5a743894a0e4a801fc3', 2, 'admin2@gmail.com'),
-(3, 'noval assalam', '40be4e59b9a2a2b5dffb918c0e86b3d7', 2, 'naufalassalam@gmail.com'),
-(5, 'tyttyt', '40be4e59b9a2a2b5dffb918c0e86b3d7', 2, 'naufalassalam.game@gmail.com'),
-(6, 'dwdwd', '40be4e59b9a2a2b5dffb918c0e86b3d7', 1, 'adwd@gmial.com'),
-(7, 'dwdw', '40be4e59b9a2a2b5dffb918c0e86b3d7', 2, 'yuiyi@gnm.com');
+INSERT INTO `user` (`id_user`, `nama_user`, `password_user`, `id_kota`, `email_user`, `level`) VALUES
+(1, 'tami', '21232f297a57a5a743894a0e4a801fc3', 1, 'super@gmail.com', '1'),
+(8, 'admin', '21232f297a57a5a743894a0e4a801fc3', 1, 'admin@gmail.com', '2'),
+(9, '', '40be4e59b9a2a2b5dffb918c0e86b3d7', 0, '', '1'),
+(10, '', '40be4e59b9a2a2b5dffb918c0e86b3d7', 0, '', '1');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `datas`
+-- Indeks untuk tabel `comvis`
+--
+ALTER TABLE `comvis`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
+
+--
+-- Indeks untuk tabel `datas`
 --
 ALTER TABLE `datas`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `kota`
+-- Indeks untuk tabel `kota`
 --
 ALTER TABLE `kota`
   ADD PRIMARY KEY (`id_kota`),
   ADD UNIQUE KEY `nama` (`nama_kota`);
 
 --
--- Indexes for table `periode`
+-- Indeks untuk tabel `periode`
 --
 ALTER TABLE `periode`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `report`
+-- Indeks untuk tabel `report`
 --
 ALTER TABLE `report`
   ADD PRIMARY KEY (`idreport`);
 
 --
--- Indexes for table `user`
+-- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `datas`
+-- AUTO_INCREMENT untuk tabel `comvis`
+--
+ALTER TABLE `comvis`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
+
+--
+-- AUTO_INCREMENT untuk tabel `datas`
 --
 ALTER TABLE `datas`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
--- AUTO_INCREMENT for table `kota`
+-- AUTO_INCREMENT untuk tabel `kota`
 --
 ALTER TABLE `kota`
-  MODIFY `id_kota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_kota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
--- AUTO_INCREMENT for table `periode`
+-- AUTO_INCREMENT untuk tabel `periode`
 --
 ALTER TABLE `periode`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
--- AUTO_INCREMENT for table `report`
+-- AUTO_INCREMENT untuk tabel `report`
 --
 ALTER TABLE `report`
   MODIFY `idreport` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
